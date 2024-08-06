95abffbfe87ac6ca8fd6e3c9b3d4b19a5496d823
RockerHX <rockerhx@gmail.com>
2024-08-06 10:57:13 +0800

    Update Package.swift
    Add targets

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Acones",
-     platforms: [
-        .iOS(.v13),
-     ],
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -22,7 +22,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Acones"),
+            name: "Acones",
+            dependencies: ["Kingfisher", "SwiftDraw"]
+        ),
         .testTarget(
             name: "AconesTests",
             dependencies: ["Acones"]),

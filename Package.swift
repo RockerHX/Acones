1abb8ebbbe3bb9c47c4f502fb9c12bdce9645506
RockerHX <rockerhx@gmail.com>
2024-08-06 10:24:51 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "Acones",
+     platforms: [
+        .iOS(.v13),
+     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Acones",
             targets: ["Acones"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swhitty/SwiftDraw.git", from: "0.17.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.12.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
